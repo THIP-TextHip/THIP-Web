9b685ea469a0196f3d162b188532fe6647913105
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-07-19 17:03:48 +0900

    feat: add MessageInputBox to FeedDetailPage

--- src/pages/feed/FeedDetailPage.tsx
+++ src/pages/feed/FeedDetailPage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import TitleHeader from '@/components/common/TitleHeader';
 import FeedPost from '@/components/feed/FeedPost';
@@ -6,13 +7,22 @@ import leftArrow from '../../assets/common/leftArrow.svg';
 import moreIcon from '../../assets/common/more.svg';
 import ReplyList from '@/components/common/Post/ReplyList';
 import { mockFeedPost, mockCommentList } from '@/data/postData';
+import MessageInput from '@/components/today-words/MessageInput';
 
 const FeedDetailPage = () => {
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
   const handleBackClick = () => {
     navigate(-1);
   };
   const handleMoreClick = () => {};
+
+  const handleSend = () => {
+    if (!message.trim()) return;
+    console.log('작성한 댓글 내용:', message);
+    // 댓글 등록 API 호출 추가
+    setMessage('');
+  };
 
   return (
     <Wrapper>
@@ -24,6 +34,12 @@ const FeedDetailPage = () => {
       />
       <FeedPost {...mockFeedPost} />
       <ReplyList commentList={mockCommentList} />
+      <MessageInput
+        placeholder="여러분의 생각을 남겨주세요"
+        value={message}
+        onChange={setMessage}
+        onSend={handleSend}
+      />
     </Wrapper>
   );
 };
