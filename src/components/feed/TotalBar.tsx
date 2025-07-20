0834f0fe122c20d36e5c5fc0045e9f44ac5148f9
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-07-20 18:40:08 +0900

    fix: TotalBar 반응형 적용

--- src/components/feed/TotalBar.tsx
+++ src/components/feed/TotalBar.tsx
@@ -1,22 +1,22 @@
 import styled from '@emotion/styled';
 
 const Container = styled.div`
+  width: 100%;
+  min-width: 320px;
+  max-width: 540px;
+  padding: 0 20px;
+  margin: 0 auto;
+`;
+
+const Inner = styled.div`
   display: flex;
   flex-direction: row;
-  width: 100%;
-  min-width: 280px;
-  max-width: 500px;
-  padding-bottom: 4px;
-  border-bottom: 1px solid #282828;
-  margin: 0 auto;
   gap: 2px;
   color: var(--color-grey-100);
   font-size: var(--font-size-sm);
   font-weight: var(--font-weight-medium);
   line-height: 24px;
-
-  .total {
-  }
+  border-bottom: 1px solid #282828;
 
   .count {
     text-align: right;
@@ -30,8 +30,10 @@ interface TotalBarProps {
 const TotalBar = ({ count }: TotalBarProps) => {
   return (
     <Container>
-      <div className="total">전체</div>
-      <div className="count">{count}</div>
+      <Inner>
+        <div className="total">전체</div>
+        <div className="count">{count}</div>
+      </Inner>
     </Container>
   );
 };
