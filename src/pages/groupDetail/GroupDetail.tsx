52e71d11257861b4156ddde477aca603405f490f
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-07-20 16:49:39 +0900

    feat: artBackground

--- src/pages/groupDetail/GroupDetail.tsx
+++ src/pages/groupDetail/GroupDetail.tsx
@@ -11,26 +11,27 @@ import rightChevron from '../../assets/common/right-Chevron.svg';
 import { colors, typography } from '@/styles/global/global';
 import { GroupCard } from '@/components/group/GroupCard';
 import artBackground from '../../assets/genre/artBackground.svg';
-import humanityBackground from '../../assets/genre/humanityBackgroud.svg';
-import ScienceITBackground from '../../assets/genre/ScienceITBackground.svg';
-import literatureBackground from '../../assets/genre/literatureBackground.svg';
-import socialScienceBackground from '../../assets/genre/socialScienceBackground.svg';
+// import humanityBackground from '../../assets/genre/humanityBackgroud.svg';
+// import ScienceITBackground from '../../assets/genre/ScienceITBackground.svg';
+// import literatureBackground from '../../assets/genre/literatureBackground.svg';
+// import socialScienceBackground from '../../assets/genre/socialScienceBackground.svg';
 
 const getBackgroundImageByGenre = (genre: string): string => {
-  switch (genre) {
-    case '문학':
-      return literatureBackground;
-    case '사회과학':
-      return socialScienceBackground;
-    case '과학•IT':
-      return ScienceITBackground;
-    case '예술':
-      return artBackground;
-    case '인문학':
-      return humanityBackground;
-    default:
-      return literatureBackground;
-  }
+  //   switch (genre) {
+  //     case '문학':
+  //       return literatureBackground;
+  //     case '사회과학':
+  //       return socialScienceBackground;
+  //     case '과학•IT':
+  //       return ScienceITBackground;
+  //     case '예술':
+  //       return artBackground;
+  //     case '인문학':
+  //       return humanityBackground;
+  //     default:
+  //       return literatureBackground;
+  //   }
+  return artBackground;
 };
 
 const GroupDetail = () => {
