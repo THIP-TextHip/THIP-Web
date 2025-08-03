262da2b43b46135007cb02b5f7982be0b2564445
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-08-03 23:46:47 +0900

    design: 반응형 적용2

--- src/components/feed/TotalFeed.tsx
+++ src/components/feed/TotalFeed.tsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 
 const EmptyState = styled.div`
   display: flex;
-  height: 473px;
+  height: 100%;
   padding: 32px 0 20px 0;
   justify-content: center;
   align-items: center;
