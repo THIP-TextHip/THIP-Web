4141633d2dffb821525dc042eccaf19e4d0cff58
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-09-12 15:57:02 +0900

    fix: 라우터 파일 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import Router from './pages';
 import { Global } from '@emotion/react';
 import { globalStyles } from './styles/global/global';
 import PopupContainer from './components/common/Modal/PopupContainer';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { sendPageView } from './lib/ga';
 
 const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    sendPageView(location.pathname);
-  }, [location.pathname]);
   return (
     <>
       <Global styles={globalStyles} />
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,7 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import ScrollToTop from './ScrollToTop';
+import { sendPageView } from '@/lib/ga';
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    sendPageView(location.pathname + location.search + location.hash);
+  }, [location.pathname, location.search, location.hash]);
+
   return (
     <>
       <ScrollToTop />
