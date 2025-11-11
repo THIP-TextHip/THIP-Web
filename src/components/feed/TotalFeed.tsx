2ad171b585227ee26e16b0f4e35cd63e48afbe40
fr0gydev <ljh130334@naver.com>
2025-11-11 17:42:49 +0900

    Revert "Merge pull request #284 from THIP-TextHip/feat/influencer"
    This reverts commit 154caf75896eda61910b2f2585834ced774a092e, reversing
    changes made to 8d5414e1a66ff03f3acfcb50f76437ad6e4de853.

--- src/components/feed/TotalFeed.tsx
+++ src/components/feed/TotalFeed.tsx
@@ -1,29 +1,25 @@
 import styled from '@emotion/styled';
-import { Fragment } from 'react';
 import FollowList from './FollowList';
 import FeedPost from './FeedPost';
-import RecommendedFeedSection from './RecommendedFeedSection';
 import type { FeedListProps } from '../../types/post';
 import { colors, typography } from '@/styles/global/global';
 
-const TotalFeed = ({ showHeader, posts = [], isTotalFeed }: FeedListProps) => {
+const TotalFeed = ({ showHeader, posts = [], isTotalFeed, isLast = false }: FeedListProps) => {
   const hasPosts = posts.length > 0;
 
   return (
     <Container>
       <FollowList />
       {hasPosts ? (
-        <>
-          {posts.map((post, index) => (
-            <Fragment key={`${post.feedId}-${index}`}>
-              <FeedPost showHeader={showHeader} isMyFeed={isTotalFeed} isLast={false} {...post} />
-              {/* 10개마다 추천 섹션 반복 표시 */}
-              {(index + 1) % 10 === 0 && (
-                <RecommendedFeedSection sectionIndex={Math.floor((index + 1) / 10) - 1} />
-              )}
-            </Fragment>
-          ))}
-        </>
+        posts.map((post, index) => (
+          <FeedPost
+            key={`${post.feedId}-${index}`}
+            showHeader={showHeader}
+            isMyFeed={isTotalFeed}
+            isLast={isLast && index === posts.length - 1}
+            {...post}
+          />
+        ))
       ) : (
         <EmptyState>
           <div>피드에 작성된 글이 없어요</div>
