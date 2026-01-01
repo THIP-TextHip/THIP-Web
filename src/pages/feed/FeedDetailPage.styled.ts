3d064c49c80deaf5604f74f4d06c23f82ccbe236
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2026-01-01 21:01:17 +0900

    refactor: FeedDetailPage 페이지 스타일 분리
    - FeedDetailPage.styled.ts 파일 생성
    - styled 컴포넌트를 별도 파일로 분리하여 코드 구조 개선

---
+++ src/pages/feed/FeedDetailPage.styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  min-width: 320px;
+  max-width: 767px;
+  min-height: 100vh;
+  padding-top: 56px;
+  margin: 0 auto;
+  background-color: #121212;
+`;
