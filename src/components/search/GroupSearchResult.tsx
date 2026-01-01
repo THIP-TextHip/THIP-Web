3cd7cff917b13891643f0daa20a4451946e37d06
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2026-01-02 01:10:05 +0900

    fix: search 폴더 내 컴포넌트의 스타일 파일 분리

--- src/components/search/GroupSearchResult.tsx
+++ src/components/search/GroupSearchResult.tsx
@@ -1,9 +1,19 @@
-import styled from '@emotion/styled';
 import { useMemo } from 'react';
 import { GroupCard } from '../group/GroupCard';
-import { colors, typography } from '@/styles/global/global';
 import { Filter } from '../common/Filter';
 import type { SearchRoomItem } from '@/api/rooms/getSearchRooms';
+import {
+  TabContainer,
+  Tab,
+  Content,
+  GroupCardHeader,
+  GroupNum,
+  EmptyContent,
+  EmptyMainText,
+  EmptySubText,
+  LoadingText,
+  ErrorText,
+} from './GroupSearchResult.styled';
 
 const FILTER = ['마감임박순', '인기순'];
 const CATEGORIES = ['전체', '문학', '과학·IT', '사회과학', '인문학', '예술'] as const;
@@ -115,85 +125,3 @@ const GroupSearchResult = ({
 };
 
 export default GroupSearchResult;
-
-const TabContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 12px;
-  padding: 0 20px;
-  margin-bottom: 16px;
-`;
-
-const Tab = styled.button<{ selected?: boolean }>`
-  white-space: nowrap;
-  padding: 8px 12px;
-  font-size: ${typography.fontSize.xs};
-  font-weight: ${typography.fontWeight.regular};
-  border: none;
-  border-radius: 16px;
-  background: ${({ selected }) => (selected ? `${colors.purple.main}` : `${colors.darkgrey.main}`)};
-  color: #fff;
-  cursor: pointer;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow-y: auto;
-  padding: 0 20px 60px;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-`;
-const GroupCardHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 20px;
-  margin-bottom: 10px;
-`;
-
-const GroupNum = styled.span`
-  display: flex;
-  align-items: center;
-  color: ${colors.white};
-  font-size: ${typography.fontSize.sm};
-  font-weight: ${typography.fontWeight.medium};
-`;
-
-const EmptyContent = styled.div`
-  display: flex;
-  height: 60vh;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-`;
-
-const EmptyMainText = styled.p`
-  color: ${colors.white};
-  font-size: ${typography.fontSize.lg};
-  font-weight: ${typography.fontWeight.semibold};
-  text-align: center;
-`;
-
-const EmptySubText = styled.p`
-  color: ${colors.grey[100]};
-  font-size: ${typography.fontSize.sm};
-  font-weight: ${typography.fontWeight.regular};
-  text-align: center;
-`;
-
-const LoadingText = styled.p`
-  color: ${colors.grey[100]};
-  font-size: ${typography.fontSize.sm};
-  text-align: center;
-`;
-
-const ErrorText = styled.p`
-  color: #ff6b6b;
-  font-size: ${typography.fontSize.sm};
-  text-align: center;
-`;
