fdddd94a1576e897a5671e52062ec7b97ef74f8e
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2026-01-01 20:53:22 +0900

    refactor: Feed 페이지 스타일 분리
    - Feed.styled.ts 파일 생성
    - styled 컴포넌트를 별도 파일로 분리하여 코드 구조 개선

---
+++ src/pages/feed/Feed.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  min-width: 320px;
+  max-width: 767px;
+  height: 100vh;
+  background-color: var(--color-black-main);
+  margin: 0 auto;
+`;
